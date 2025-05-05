ed1736ce8c42e0c40fb8ef77d5ae08c0e7c35692
MohamedSalah54 <mohamed.salah199988@gmail.com>
2025-05-05 16:33:15 +0300

    category setup

--- src/admin/category/category.service.ts
+++ src/admin/category/category.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CategoryRepo } from '../../db/models/category/category.repo';
-import { TCategory } from '../../db/models/category/category.model';
 
-interface ICreateCategory {
-  name: string;
-  description?: string;
-}
 
 @Injectable()
 export class CategoryService {
