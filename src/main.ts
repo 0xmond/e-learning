bd4036e020371075ca8625e6f2dfb475670be2d3
0xmond <mostafamahmoud12120@gmail.com>
2025-05-05 16:05:48 +0300

    Completed Authentication Module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
+import { config } from 'dotenv';
+config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
-  const port = configService.get<number>('PORT') || 3001
-    await app.listen(port);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
